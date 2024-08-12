b31f5d03f748e33fb503ee669468e937a4ce0163
Léo Turcat <turcatleo@gmail.com>
2024-08-12 16:09:33 +0200

    nouvelles routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "offers#index"
+  resources :offers, only: [:index, :new, :create, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:edit, :update]
 end
