ffe748df0ac8b7fd4c758d4bdf7bc1b04bb3bce8
Léo Turcat <turcatleo@gmail.com>
2024-08-13 14:34:44 +0200

    new & create offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,26 @@
 class OffersController < ApplicationController
+  # before_action :set_offer, only: [:show]
+def new
+  @offer = Offer.new
+end
+
+def create
+  @offer = Offer.new(offer_params)
+  @offer.list = @offer
+  if @offer.save
+    redirect_to offer_path(@offer)
+  else
+    render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :speciality, :price, :user_id)
+  end
+
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
+end
 end
