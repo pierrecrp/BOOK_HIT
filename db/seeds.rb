10e003ee4f191514bb5dccad7003f2a8da315398
Pierre Crépin <pierrecrepin33200@gmail.com>
2024-08-13 13:01:20 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Offer.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+
+attributes = [
+  { email: "fake@gmail.com", password: "vnoened" },
+  
+]
+
+user = User.create!(email: "example@gmail.com", password: "zcjben")
+
+
+puts "Creating offers..."
+attributes = [
+  { title: "Botanical Butcher", description: "Uses only organic, plant-based poisons. Eco-friendly elimination!", speciality: "poison", price: "1500" },
+  { title: "Mother Nature’s Mistake", description: "Arranges for unfortunate 'natural' disasters like falling trees or rockslides.", speciality: "environmental", price: "2599" }
+ 
+]
+attributes.each do |attribute|
+  offer = Offer.new(attribute)
+  offer.user = user
+  puts "Created #{offer.title}"
+end
+puts "Finished!"
