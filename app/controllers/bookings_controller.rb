21364b7af8f03b61adb10836ae78d01b9ebb8705
Léo Turcat <turcatleo@gmail.com>
2024-08-13 15:47:28 +0200

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,44 @@
 class BookingsController < ApplicationController
+  before_action :set_list, only: [:new, :create, :edit, :update]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.list = @offer
+    if @booking.save
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to offer_path(@offer), notice: 'Booking was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to order_path(@booking.list), status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:target, :deadline, :accepted, :user_id, :offer_id)
+  end
+
+  def set_offer
+    @offer = Offer.find(params[:offer_id])
+  end
 end
